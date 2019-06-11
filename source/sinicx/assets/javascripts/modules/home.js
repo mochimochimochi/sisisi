4b0c72eee78a25417367159394f46790a2ffcdc7
delights-imac2018-01 <kawamura@delights.co.jp>
2019-06-11 17:42:08 +0900

    refactor: 不要コード削除

--- source/sinicx/assets/javascripts/modules/home.js
+++ source/sinicx/assets/javascripts/modules/home.js
@@ -1,11 +1,11 @@
 const home = function() {
 
   // members
+
   let $tgt = $('#membersList li'),
   $tgtAc = $('#membersList li.is-active'),
   $len = $tgt.length;
-
-  /** 重複チェック用配列 */
+  // 乱数用
   let $randoms = [];
   let $min = 1, $max = $len;
   for(i = $min; i <= $max; i++){
@@ -17,17 +17,14 @@ const home = function() {
       }
     }
   }
-  /** $min以上$max以下の整数値の乱数を返す */
   function intRandom(min, max){
-    return Math.floor( Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  console.log($randoms)
-
+  // 表示用
   $tgt.removeClass('is-active');
   for(i=0;i<3;i++){
     let $num = $randoms[i] - 1;
-    console.log($num)
     $tgt.eq($num).addClass('is-active');
   }
 
